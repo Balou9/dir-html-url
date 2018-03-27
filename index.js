6ef6b90a0a9b0ea86f67976f540f8ef989569d29
Balou9 <emmanuel.owosekun@gmail.com>
2018-03-27 23:10:39 +0200

    small fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function dirHtmlUrl(dir, callback) {
 
   dirHtmlList(dir, function (err, data) {
     if (err) return callback(err)
-    var fullHtmlPath = getAbsPath(dir, data).map(x => 'file:///' + x)
+    var fullHtmlPath = getAbsPath(dir, data).map(paths => 'file:///' + paths)
 
     function reducer (acc, cur, index) {
     acc[data[index]] = cur
